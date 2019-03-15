59ac597ebdc8ae5c58d2afdd4b5546876d321138
Ruchira Swarnapriya <ruchira.swarnapriya8@gmail.com>
2019-03-15 09:50:36 +0530

    Modify Time Table

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { DayService, WeekService, WorkWeekService, MonthService, AgendaService } from '@syncfusion/ej2-angular-schedule';
-
+import { DayService, WeekService, WorkWeekService, MonthService, AgendaService,TimeScaleModel,EventSettingsModel } from '@syncfusion/ej2-angular-schedule';
+import { scheduleData } from '../datasource';
 
 @Component({
   selector: 'app-table-list',
@@ -10,6 +10,10 @@ import { DayService, WeekService, WorkWeekService, MonthService, AgendaService }
 
 })
 export class TableListComponent implements OnInit {
+  public selectedDate: Date = new Date(2018, 1, 15);
+  public timeScale: TimeScaleModel = { enable: true, interval: 60, slotCount: 1 };
+  public eventSettings: EventSettingsModel = { dataSource: scheduleData };
+ 
 
   constructor() { }
 
