f5a4f99efba006bae1ee18130b706bd3a989a7f6
unknown <Ruwaliya>
2019-04-20 16:29:23 +0530

    workload

---
+++ src/app/workload/halls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-halls',
+  templateUrl: './halls.component.html',
+  styleUrls: ['./halls.component.scss']
+})
+export class HallsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
