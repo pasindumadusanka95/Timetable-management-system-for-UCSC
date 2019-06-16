f5de7ff10fb5e23329dd991d45bc7f788a9176e5
Pasindu Madusanka <pasindusenerath@gmail.com>
2019-06-16 12:18:29 +0530

    modified admin notification format

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from 'app/shared/messages.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ToastrService } from 'ngx-toastr';
 import { Subjects } from 'app/shared/subjects.model';
+import { NotificationsService } from 'app/shared/notifications.service';
 
 
 @Component({
@@ -14,37 +15,37 @@ import { Subjects } from 'app/shared/subjects.model';
 export class MessagesComponent implements OnInit {
   subjects: Subjects[];
   constructor(public service: MessageService, private firestore: AngularFirestore,
-    private toastr : ToastrService) {}
- 
+    private toastr: ToastrService,public notiservice: NotificationsService) {}
+
   ngOnInit() {
     this.resetForm();
     this.service.getSubjects().subscribe(actionArray => {
-      this.subjects = actionArray.map(item=>{
+      this.subjects = actionArray.map(item => {
         return {
           id: item.payload.doc.id,
-          ...item.payload.doc.data() 
+          ...item.payload.doc.data()
         } as Subjects;
-        
+
         })
   });
   }
- 
- 
+
+
   resetForm(form?: NgForm) {
     // tslint:disable-next-line:curly
     if (form != null)
       form.resetForm();
     this.service.formData = {
       id: null,
-      LecturerID:'',
-      Date:'',
-      Time:'',
-      NewDate:'',
-      NewTime:'',
+      LecturerID: '',
+      Date: '',
+      Time: '',
+      NewDate: '',
+      NewTime: '',
       Subject: '',
       Reason: '',
-      Message:''
-      
+      Message: ''
+
     }
   }
 
@@ -55,22 +56,27 @@ export class MessagesComponent implements OnInit {
     showSuccessMessage: boolean;
     // tslint:disable-next-line:member-ordering
     formControls = this.service.form.controls;
-  
 
 
-  onSubmit(form:NgForm){
-    let data = Object.assign({},form.value);
-  //  console.log(data.Subject);
-    data.message= data.LecturerID + " requested rechedule for "+data.Subject+" on "+ data.Date +" at "+data.Time+ ". requesting new date on "+data.NewDate+" at "+data.NewTime+".";
-    delete data.id;
+
+  onSubmit(form: NgForm) {
+    const data = Object.assign({}, form.value);
+   console.log(data);
+  // tslint:disable-next-line:max-line-length
+  // this.notiservice.formData.notification_body = data.LecturerID + ' requested rechedule for ' + data.Subject + ' on ' + data.Date + ' at ' + data.Time + '. requesting new date on ' + data.NewDate + ' at ' + data.NewTime + '.';
+  // this.notiservice.formData.type=2;
+  // this.notiservice.formData.notification_subject="recheduling request";
+  const notificationdata = Object.assign({}, this.notiservice.formData);
+  delete data.id;
     // tslint:disable-next-line:curly
-    if(form.value.id == null)
+    if (form.value.id == null) {
       this.firestore.collection('messages').add(data);
-    // tslint:disable-next-line:curly
-    else
-    this.firestore.doc('messages/'+ form.value.id).update(data);
+      this.firestore.collection('notifications').add(notificationdata);
+    } else {
+    this.firestore.doc('messages/' + form.value.id).update(data);
+    }
       this.resetForm(form);
-      this.toastr.success('successfully','Message Sent');
+      this.toastr.success('successfully', 'Message Sent');
   }
 
   onChange(newValue) {
---
+++ src/app/shared/notifications.model.ts
@@ -0,0 +1,6 @@
+export interface Notifications {
+    id: string;
+    type:number;
+    notification_subject: string;
+    notification_body:string ;
+}
