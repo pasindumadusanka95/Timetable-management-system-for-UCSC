f9b4d4853b490a81a8d39c46937de7580a66519e
Ruwanthi Liyanage <jn.ruwanthiliyanage@gmail.com>
2019-08-14 10:49:48 +0530

    Upcoming Lectures in Dashbord

--- src/app/viewUpcomingLectures/view-upcoming-first-year/view-upcoming-first-year.component.ts
+++ src/app/viewUpcomingLectures/view-upcoming-first-year/view-upcoming-first-year.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TimeTableCRUDService } from 'app/shared/time-table-crud.service';
 
 @Component({
   selector: 'app-view-upcoming-first-year',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUpcomingFirstYearComponent implements OnInit {
 
-  constructor() { }
+  public time ;
+  public location;
+  public startTime;
+  public Subject;
+
+  constructor(private ttcs:TimeTableCRUDService,) {
+    let date = new Date();
+    this.time = date.getTime(); 
+    
+    console.log(date.getTime())
+   }
 
   ngOnInit() {
+
+    this.ttcs.getFirstYearTT().subscribe(next=>{
+
+      for (let i of next.data().firstyear as any[]){
+        let st1 = i.StartTime.toDate();
+        let st2 = st1.getTime();
+        if (this.time < st2){
+          this.Subject = i.Subject
+          this.location = i.location
+          this.startTime = i.StartTime.toDate()
+          console.log( this.Subject,
+            this.location,
+            this.startTime )
+        }
+       
+       
+        
+        
+
+      }
+    
+     
+     
+    })
   }
 
 }
